d434bbcba1d436f59a79d2b39cf0fc48dc312ffb
AAusa <95350078+AAusa@users.noreply.github.com>
2022-10-14 12:56:47 +0200

    Video2Terminado

---
+++ Player.gd
@@ -0,0 +1,38 @@
+extends Area2D
+export (int) var Velocidad
+var Movimiento = Vector2()
+var limite
+
+func _ready():
+	limite = get_viewport_rect().size
+	
+
+
+func _process(delta):
+	Movimiento = Vector2() # Reiniciar el movimiento despues de pulsar
+	# Movimientos al pulsar teclas:
+	if Input.is_action_pressed("ui_right"):
+		Movimiento.x += 1
+	if Input.is_action_pressed("ui_left"):
+		Movimiento.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		Movimiento.y += 1
+	if Input.is_action_pressed("ui_up"):
+		Movimiento.y -= 1
+		
+	if Movimiento.length() > 0: # Verifica si se está moviendo
+		Movimiento = Movimiento.normalized() * Velocidad # Normalilza la velocidad
+
+	position += Movimiento * delta # delta permite que el timing en el juego sea el mismo en cualquier ordenador
+	position.x = clamp(position.x, 0, 490) #Limites de pantalla
+	position.y = clamp(position.y, 310, 1010)
+	
+	if Movimiento.x != 0: # Si se mueve en el eje x sprite lado
+		$Sprite_player.animation = "lado"
+		$Sprite_player.flip_h = Movimiento.x > 0 # Si se mueve a la izq. se voltea el sprite horizontalmente
+		$Sprite_player.flip_v = false
+	elif Movimiento.y != 0: # Si se mueve en el eje y sprite espalda
+		$Sprite_player.animation = "espalda"
+		$Sprite_player.flip_v = Movimiento.y > 0# Si se mueve hacia arriba se voltea el sprite verticalmente
+	else: # Si se mueve en el eje x sprite lado
+		$Sprite_player.animation = "frente"
